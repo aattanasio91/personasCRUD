3db0ec70db169a647ed84c0e089a7eff93007238
aattanasio91 <agustin.attanasio@gmail.com>
2022-08-04 00:49:50 -0300

    Cambios para Heroku

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS public.relation_type;
 DROP TABLE IF EXISTS public.person;
 DROP TABLE IF EXISTS public.country;
 
-CREATE TABLE country (
+CREATE TABLE public.country (
      id serial4 NOT NULL,
      name varchar(80),
      CONSTRAINT country_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE person (
+CREATE TABLE public.person (
     id serial4 NOT NULL,
     birth_date date NULL,
     country_id int4 NULL,
@@ -20,20 +20,20 @@ CREATE TABLE person (
     "name" varchar(255) NULL,
     phone_number varchar(255) NULL,
     CONSTRAINT person_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_person_country FOREIGN KEY (country_id) REFERENCES country(id)
+    CONSTRAINT fk_person_country FOREIGN KEY (country_id) REFERENCES public.country(id)
 );
 
-CREATE TABLE relation_type (
+CREATE TABLE public.relation_type (
       id serial4 NOT NULL,
       relation_type varchar(255) NULL,
       CONSTRAINT relation_type_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE relation (
+CREATE TABLE public.relation (
      id serial4 NOT NULL,
      id_person1 int4 NULL,
      id_person2 int4 NULL,
      relation_type_id int4 NULL,
     CONSTRAINT relation_pkey PRIMARY KEY (id),
-    CONSTRAINT FK_RELATION_RELATION_TYPE FOREIGN KEY (relation_type_id) REFERENCES relation_type(id)
+    CONSTRAINT FK_RELATION_RELATION_TYPE FOREIGN KEY (relation_type_id) REFERENCES public.relation_type(id)
 );
